fa68ed88e0133708f589311b2842113fe45d1975
haihua.gu <guhaihua0507@163.com>
2020-05-18 21:37:01 +0800

    update

--- xingyutang/src/main/java/com/xingyutang/config/GlobalExceptionHandler.java
+++ xingyutang/src/main/java/com/xingyutang/config/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.xingyutang.app.config;
+package com.xingyutang.config;
 
 import com.xingyutang.app.model.vo.ResponseData;
 import com.xingyutang.exception.GlobalResponseException;
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -21,7 +22,11 @@ public class GlobalExceptionHandler {
             GlobalResponseException ex = (GlobalResponseException)e;
             return ResponseData.error(ex.getCode(), ex.getMessage());
         } else {
-            return ResponseData.error(1, e.getMessage());
+            String errorMessage = e.getMessage();
+            if (e instanceof MaxUploadSizeExceededException) {
+                errorMessage = "文件太大，无法上传";
+            }
+            return ResponseData.error(1, errorMessage);
         }
     }
 }
